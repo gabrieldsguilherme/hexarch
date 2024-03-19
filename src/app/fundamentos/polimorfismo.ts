8d86ff01d5b373e936302b690ef4aee2d97f45a3
GG <gabriel.guilherme@sensedia.com>
2024-03-18 22:58:07 -0300

    implementando carros e permitindo acelerar e frear

--- src/app/fundamentos/polimorfismo.ts
+++ src/app/fundamentos/polimorfismo.ts
@@ -1,12 +1,24 @@
+import Ferrari from "@/core/fundamentos/Ferrari";
 import TerminalUtil from "../util/TerminalUtil"
+import Carro from "@/core/fundamentos/Carro";
+import Fusca from "@/core/fundamentos/Fusca";
 
 export default async function polimorfismo() {
     TerminalUtil.titulo("Polimorfismo");
 
-    const tipoCarro = await TerminalUtil.selecao("Qual o tipo de carro?", ["Hatch", "Sedan", "SUV"]);
+    const [tipoCarro] = await TerminalUtil.selecao("Qual o tipo de carro?", ["Ferrari", "Fusca"]);
+
+    const carro: Carro = tipoCarro === 0 ? new Ferrari() : new Fusca();
 
     while (true) {
         TerminalUtil.limpar()
+        TerminalUtil.exibirChaveValor("Velocidade Máxima: ", `${carro.velocidadeMaxima} km/h`)
+        TerminalUtil.exibirChaveValor("Velocidade Atual: ", `${carro.velocidadeAtual} km/h`)
+
+        const [opcao] = await TerminalUtil.selecao("Qual opção?", ["Acelerar", "Frear"])
+
+        opcao === 0 ? carro.acelerar() : carro.frear()
+
         const continuar = await TerminalUtil.confirmacao("Deseja continuar?")
         if (!continuar) return
     }
---
+++ src/core/fundamentos/Carro.ts
@@ -0,0 +1,8 @@
+export default interface Carro {
+    
+    readonly velocidadeMaxima: number
+    velocidadeAtual: number
+    acelerar(): void
+    frear(): void
+
+}
